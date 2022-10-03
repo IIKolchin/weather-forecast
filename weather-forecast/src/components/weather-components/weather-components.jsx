0c98e8e4e6e6e6926bc2a5947ffce1b92cb5b093
Apple <IIKolchin@yandex.ru>
2022-10-03 03:57:46 +0300

    add new weather api, create forecast for a week

--- weather-forecast/src/components/weather-components/weather-components.jsx
+++ weather-forecast/src/components/weather-components/weather-components.jsx
@@ -1,37 +1,31 @@
 import styles from './weather-components.module.css';
-import {getCoord, getWeatherRequest} from '../../utils/api';
-import {useRef, useEffect} from 'react';
+import { getCoord, getWeatherRequest } from '../../utils/api';
+import { useRef, useEffect } from 'react';
+import fromUnixTime from 'date-fns/fromUnixTime';
+import { format, addMinutes, parseISO } from 'date-fns';
+import cloud from '../../images/cloud_small.png';
+import wind from '../../images/wind_speed.png';
+import humidity from '../../images/humidity.png';
+import sun from '../../images/sun.png';
+import sun_small from '../../images/sun.svg';
+import rain_small from '../../images/rain.svg';
+import cloud_small from '../../images/cloud.svg';
 
-export default function WeatherComponents() {
-    const ref = useRef(null);
+export default function WeatherComponents({
+  latitude,
+  longitude,
+  dataNow,
+  dataWeek,
+}) {
+  const sunrise = fromUnixTime(dataNow?.sys.sunrise).toString().slice(16, 21);
+  const sunset = fromUnixTime(dataNow?.sys.sunset).toString().slice(16, 21);
 
-    useEffect(() => {
-        getPosition()
-      })
+  useEffect(() => {});
+  // "2022-10-03T03:00:00"
+  const day = dataWeek?.timestamp_utc;
+  const image = console.log(day);
 
-       const getPosition = () => {
-
-     navigator.geolocation.getCurrentPosition(function(position) {
-            console.log(position.coords.latitude, position.coords.longitude);
-            getWeatherRequest(position.coords.latitude, position.coords.longitude).then((data) => {
-                console.log(data);
-              });
- const pic = `https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d576587.6582438038!2d${position.coords.longitude}!3d${position.coords.latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1652100284601!5m2!1sru!2sru`
-       
-console.log(pic)
-               
-       ref.current.setAttribute('src', pic);
-            //   iframe.setAttribute(
-            //     "src",
-            //     `https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d576587.6582438038!2d${position.coords.longitude}!3d${position.coords.latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1652100284601!5m2!1sru!2sru`
-            //   );
-          });
- 
-
-    }
-
-    
- 
+  const map = `https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d576587.6582438038!2d${longitude}!3d${latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1652100284601!5m2!1sru!2sru`;
 
   return (
     <section className={styles.section}>
@@ -40,33 +34,60 @@ console.log(pic)
         {/* <button className={styles.button}>Найти</button> */}
       </form>
       <div className={styles.week}>
-        <div className={styles.item}>
-            <p className={styles.day}></p>
-            <img src='' alt=''/>
-            <p className={styles.temp}></p>
-        </div>
+        {dataWeek &&
+          dataWeek.data.map((dataWeek) => {
+            return (
+              <div className={styles.item}>
+                <p className={styles.day}>
+                  {format(parseISO(dataWeek.timestamp_utc), 'eee HH:mm')}
+                </p>
+                <img
+                  className={styles.svg}
+                  src={
+                    dataWeek.weather.description.includes('rain')
+                      ? rain_small
+                      : dataWeek.weather.description.includes('cloud')
+                      ? cloud_small
+                      : dataWeek.weather.description.includes('sun')
+                      ? sun_small
+                      : sun_small
+                  }
+                  alt=''
+                />
+                <p className={styles.temp}>{Math.floor(dataWeek.temp)}°C</p>
+              </div>
+            );
+          })}
       </div>
       <div className={styles.details}>
         <div className={styles.info}>
-            <img src='' alt='' />
-            <p className={styles.text}></p>
+          <img className={styles.img} src={cloud} alt='' />
+          <p className={styles.text}>Clouds {dataNow?.clouds.all} %</p>
         </div>
         <div className={styles.info}>
-            <img src='' alt='' />
-            <p className={styles.text}></p>
+          <img className={styles.img} src={wind} alt='' />
+          <p className={styles.text}>
+            Wind speed {Math.floor(dataNow?.wind.speed)} m/s
+          </p>
         </div>
         <div className={styles.info}>
-            <img src='' alt='' />
-            <p className={styles.text}></p>
+          <img className={styles.img} src={humidity} alt='' />
+          <p className={styles.text}>Humidity {dataNow?.main.humidity} %</p>
+        </div>
+        <div className={styles.info}>
+          <img className={styles.img} src={sun} alt='' />
+          <p className={styles.text}>
+            {sunrise} | {sunset}
+          </p>
         </div>
       </div>
       <iframe
-        ref={ref}
-       className={styles.iframe}
+        src={map}
+        className={styles.iframe}
         title='This is a unique title'
         width={660}
-        height={200}
-        style={{border: 0}}
+        height={204}
+        style={{ border: 0 }}
         allowFullScreen=''
         loading='lazy'
         referrerPolicy='no-referrer-when-downgrade'
--- weather-forecast/src/utils/api.js
+++ weather-forecast/src/utils/api.js
@@ -1,3 +1,22 @@
+const axios = require('axios');
+
+export const getWeatherRequest = async (lat, lon) => {
+  try {
+    const res = await axios.request({
+      method: 'GET',
+      url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly',
+      params: { lat: lat, lon: lon },
+      headers: {
+        'X-RapidAPI-Key': '415ef0867cmsha1e7cbc993d38eap1a4c8ajsn5da9e3d5a86a',
+        'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
+      },
+    });
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 function checkResponse(res) {
   if (res.ok) {
     return res.json();
@@ -13,7 +32,7 @@ export const getCoord = async (city) => {
   return checkResponse(res);
 };
 
-export const getWeatherRequest = async (lat, lon) => {
+export const getWeatherRequestNow = async (lat, lon) => {
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f944b0150cc655ed9449452d91d6e60c`,
     {}
